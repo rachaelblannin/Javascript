d3e55a5407fdc6a680ac6543a881a28c196114b2
rachaelblannin <rachblannin@gmail.com>
2022-09-27 11:13:21 +0100

    initial commit

---
+++ script.js
@@ -0,0 +1,29 @@
+'use strict'
+console.log ("Rach");
+console.info ("Blan");
+console.warn ("Cirencester");
+console.error ("Capricorn");
+
+let mycar ="Ford";
+let mymodel ="Pickup";
+console.log("my fav car is " + mycar + " and the model is " + mymodel);
+
+console.log("This is now %c the end of the exercises", "color: orange; font-family:fantasy; font-style:bold; background-color:black; padding:10px");
+
+
+let a;
+console.log(a);
+
+let b = "12345";
+console.log(b);
+
+let d = true;
+console.log(d);
+
+let e = {a:"javascript"};
+console.log(e);
+
+let totalMoney = 4000;
+let moneyPaidSoFar = 2348;
+let totalLeftToPay = totalMoney - moneyPaidSoFar;
+console.log(`The total bill is £${totalMoney} the remaining amount of money to be paid is £${totalLeftToPay}`);
